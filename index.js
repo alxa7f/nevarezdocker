51c40cda7cf118adf3004808e73226de510164d9
Alejandro N <141671713+alxa7f@users.noreply.github.com>
2025-05-19 09:44:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
 http.createServer((req, res) => {
-  res.end('¡Hola desde nevarezdocke!');
+  res.end('¡Hola desde nevarezdocker!');
 }).listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
